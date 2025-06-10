34a9cf85cdbb2574376e5e4ca5a71dee7f3c5a92
Zhu Li <imzhuli@gmail.com>
2025-06-10 15:43:52 +0800

    rename

--- components/server_dirs.py
+++ components/server_dirs.py
@@ -9,12 +9,12 @@ class ServerDirs:
             self.bin = hp.joinpath("bin")
             self.conf = hp.joinpath("conf")
             self.data = hp.joinpath("data")
-            self.local_data = hp.joinpath("local_data")
+            self.cache = hp.joinpath("cache")
 
             os.makedirs(self.bin, exist_ok=True)
             os.makedirs(self.conf, exist_ok=True)
             os.makedirs(self.data, exist_ok=True)
-            os.makedirs(self.local_data, exist_ok=True)
+            os.makedirs(self.cache, exist_ok=True)
 
         except (TypeError, AttributeError, ValueError):
             return False
