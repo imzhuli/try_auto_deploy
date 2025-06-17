b07df7271326be66a805ffb35e8565f1189d199f
imzhuli <imzhuli@vip.qq.com>
2025-06-18 02:24:17 +0800

    deploy.py: add remote starting script & config.

--- deploy.py
+++ deploy.py
@@ -22,18 +22,16 @@ def DeployService(config_file):
         source = yd["source"]
         source_home = Path(os.path.abspath(source["home_dir"]))
         source_binary_dir = source_home / "service.binary"
-        source_script_dir = source_home / "service.script"
         source_config_template_dir = source_home / "service.config.template"
         source_config_temp_output_dir = source_home / "service.config.output.temp"
+        source_script_dir = source_home / "service.script"
 
-        config = source.get("config", [])
-        config_values = source.get("config_values", {})
+        source_configs = source.get("config", [])
+        source_config_values = source.get("config_values", {})
 
-        for file in config:
-            config_file_path = source_config_template_dir / file
-            print(config_file_path)
-            print(config_values)
-            sc.make_config(config_file_path, source_config_temp_output_dir, config_values)
+        for file in source_configs:
+            source_config_template_file_path = source_config_template_dir / file
+            sc.make_config(source_config_template_file_path, source_config_temp_output_dir, source_config_values)
 
         source_binaries = source["binaries"]
         source_scripts = source["scripts"]
@@ -48,19 +46,31 @@ def DeployService(config_file):
                 binaries.append(full_path)
             runner.upload_bin(binaries)
 
+            configs = []
+            for item in source_configs:
+                full_path = str(source_config_temp_output_dir / item)
+                configs.append(full_path)
+            runner.upload_config(configs)
+
             scripts = []
             for item in source_scripts:
                 full_path = str(source_script_dir / item)
                 scripts.append(full_path)
             runner.upload_script(scripts)
 
-            _, o, e = runner.run_script(install_script)
-            print("<<<<<<<<<<")
-            print(o)
-            print("----------")
-            print(e)
+            print(f'run script: {install_script} {["start"] + source_configs}')
+            _, o, e = runner.run_script(install_script, *(["stop"] + source_configs))
+            print(o, end="")
+            print(e, end="")
+            _, o, e = runner.run_script(install_script, *(["start"] + source_configs))
+            print(o, end="")
+            print(e, end="")
+            _, o, e = runner.run_script(install_script, *(["status"] + source_configs))
+            print(o, end="")
+            print(e, end="")
             pass
 
 
 if __name__ == "__main__":
+    # prepare.exec(clean=False)
     DeployService("./test_assets/server/00-server-id.yaml")
