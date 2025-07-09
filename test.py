899b5c3c47b2a5fba4f0719050dd717e93abd28e
imzhuli <imzhuli@vip.qq.com>
2025-07-09 10:02:38 +0800

    add sleep 1 between stop and start service

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ prepare.exec(clean=False)
 if __name__ == "__main__":
     # DeployService("./test_assets/server/00-s_server_id_center.yaml")
     # DeployService("./test_assets/server/01-server_config_center.yaml")
-    DeployService("./test_assets/server/02-s_server_list.yaml")
+    # DeployService("./test_assets/server/02-s_server_list.yaml")
     # DeployService("./test_assets/server/10-server_relay_device.yaml")
     # DeployService("./test_assets/server/11-server_device_state_relay.yaml")
     DeployService("./test_assets/server/12-s_auth_cache.yaml")
     # DeployService("./test_assets/server/15-s_pa.yaml")
     # DeployService("./test_assets/server/21-server_audit_device.yaml")
     # DeployService("./test_assets/server/22-server_audit_account.yaml")
-    # DeployService("./test_assets/server/99-test_server_list_client.yaml")
+    DeployService("./test_assets/server/99-t_auth_cache_client.yaml")
 
     pass
