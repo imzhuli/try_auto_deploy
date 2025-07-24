851844178ddfef0efd91127d06117ab101655df7
imzhuli <imzhuli@vip.qq.com>
2025-07-24 15:00:51 +0800

    add config templates

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from deploy import DeployService
 import test_assets.prepare as prepare
+import time
+from datetime import datetime
+from deploy import DeployService
 
 prepare.exec(clean=False)
 
 if __name__ == "__main__":
+
+    formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("当前时间:", formatted_time)
+
     ##DeployService("./test_assets/server/00-s_server_id_center.yaml")
     ##DeployService("./test_assets/server/01-s_server_list.yaml")
     ##DeployService("./test_assets/server/02-s_config_center.yaml")
@@ -11,12 +17,14 @@ if __name__ == "__main__":
     ##DeployService("./test_assets/server/10-s_device_relay.yaml")
     ##DeployService("./test_assets/server/11-s_device_state_relay.yaml")
     ##DeployService("./test_assets/server/12-s_auth_cache.yaml")
+
     ##DeployService("./test_assets/server/13-s_device_selector.yaml")
+
+    ##DeployService("./test_assets/server/14-s_audit_account.yaml")
     DeployService("./test_assets/server/15-s_pa.yaml")
 
     ## DeployService("./test_assets/server/80-r_device_audit_to_backend.yaml")
 
     # DeployService("./test_assets/server/98-t_server_list_downloader.yaml")
-    ##DeployService("./test_assets/server/99-t_auth_cache_client.yaml")
 
     pass
---
+++ test_assets/prepare.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import sys
+import components.x as x
+
+
+def exec(clean=True):
+    source_path = "/home/ubuntu/Projects/PPPro/_build/cpp/bin"
+    if not os.path.isdir(source_path):
+        x.pe("源执行文件目录不存在")
+        return False
+
+    server_app_path = "/home/ubuntu/Projects/try_auto_deploy/test_assets/service.binary"
+    if not os.path.isdir(server_app_path):
+        x.pe("目标执行文件目录不存在")
+        return False
+
+    # clean target dir
+    if clean:
+        for filename in os.listdir(server_app_path):
+            file_path = os.path.join(server_app_path, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+                print(f"Deleted directory: {file_path}")
+
+    for filename in os.listdir(source_path):
+        if not filename.startswith("s_") and not filename.startswith("t_") and not filename.startswith("r_"):
+            continue
+
+        source_filename = os.path.join(source_path, filename)
+        target_filename = os.path.join(server_app_path, filename)
+        if os.path.isfile(source_filename):
+            shutil.copy(source_filename, target_filename)
+
+    return True
